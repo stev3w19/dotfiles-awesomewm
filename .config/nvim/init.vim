1d403d244e28fc9d3bb0f9317c993f3d109dcca7
steve <stev3w19@tutanota.com>
2023-02-19 12:40:05 +0000

    lots of program configs

---
+++ .config/nvim/init.vim
@@ -0,0 +1,9 @@
+set clipboard+=unnamedplus
+
+set termguicolors
+
+call plug#begin()
+Plug 'https://github.com/vim-airline/vim-airline'
+call plug#end()
+
+
---
+++ .config/zsh/alias.zsh
@@ -0,0 +1,36 @@
+# core commands
+alias ..='cd ..'
+
+# core coreutils replacement
+alias ls='lsd -l -A'
+#alias ls='ls -lA | grep "^d" && ls -la | grep -v "^d"'
+
+# dotfiles - git bare repo
+alias dot='/usr/bin/git --git-dir=$HOME/dotfiles-awesomewm/ --work-tree=$HOME'
+
+# editor
+alias v='nvim'
+alias sv='sudoedit'
+
+# pacman
+alias pacor='sudo pacman -Qtdq | sudo pacman -Rns -'
+
+# grub
+alias updategrub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
+# cli apps
+alias ani='ani-cli -c'
+# ricing wip
+alias zshc='cd ~/.config/zsh/'
+alias wm='cd ~/.config/awesome'
+
+alias ki='v ~/.config/kitty/kitty.conf'
+alias nv='v ~/.config/nvim/init.vim'
+alias pi='v ~/.config/picom/picom.conf'
+
+alias ro='v ~/.config/rofi/config.rasi'
+
+alias cmpd='v ~/.config/mpd/mpd.conf'
+alias nc='v ~/.config/ncmpcpp/config'
+
+alias ccd='cd ~/.config/'
